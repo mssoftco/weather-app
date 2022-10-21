5cb85b05091f51247332d58fc754ba8ed47edebe
Morteza <mssoftco@yahoo.com>
2022-10-22 03:25:10 +0330

    prettier

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started 
+# Getting Started
 
 In the project directory, you can run:
 
--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
   const handleSelectCity = (event: any) => {
     setSelectedCity(event.target.value);
   };
-  const handleSelectDays = (num:number) => {
+  const handleSelectDays = (num: number) => {
     setSelectedDay(num);
   };
 
@@ -31,17 +31,17 @@ function App() {
     <div className='container-sm d-flex w-100 h-100 p-3 mx-auto flex-column'>
       <main>
         <div className='mb-auto'>
-        <nav className='nav justify-content-center'>
-          <select onChange={handleSelectCity}>
-            <option value='Tehran'>Tehran</option>
-            <option value='Shiraz'>Shiraz</option>
-            <option value='Amsterdam'>Amsterdam</option>
-            <option value='Berlin'>Berlin</option>
-            <option value='Montreal'>Montreal</option>
-          </select>
-        </nav>
-        <h1 className='display-4'>{selectedCity}</h1>
-      </div>
+          <nav className='nav justify-content-center'>
+            <select onChange={handleSelectCity}>
+              <option value='Tehran'>Tehran</option>
+              <option value='Shiraz'>Shiraz</option>
+              <option value='Amsterdam'>Amsterdam</option>
+              <option value='Berlin'>Berlin</option>
+              <option value='Montreal'>Montreal</option>
+            </select>
+          </nav>
+          <h1 className='display-4'>{selectedCity}</h1>
+        </div>
         <h1 className='display-1 fw-normal d-flex align-baseline justify-content-center'>
           {dailyWeathers ? dailyWeathers[selectedDay].temp.day?.toFixed(0) : '__'}
           <small className='fs-4'>°C</small>
@@ -57,14 +57,17 @@ function App() {
             dailyWeathers?.map(
               (day, index) =>
                 index < 7 && (
-                  <div onClick={()=>handleSelectDays(index)} className={`day d-flex flex-row flex-md-column m-1 m-lg-2 border rounded align-items-center justify-content-center p-0 p-md-2 ${index === selectedDay && 'active'}`}>
-                    <img
-                      src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
-                      className='rounded-circle'
-                      alt='Icon'
-                    />
+                  <div
+                    onClick={() => handleSelectDays(index)}
+                    className={`day d-flex flex-row flex-md-column m-1 m-lg-2 border rounded align-items-center justify-content-center p-0 p-md-2 ${
+                      index === selectedDay && 'active'
+                    }`}
+                  >
+                    <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} className='rounded-circle' alt='Icon' />
                     <p>{dayjs.unix(day.dt).toString().slice(0, 3)}</p>
-                    <p>{day.temp.min.toFixed(0)}° / {day.temp.max.toFixed(0)}°</p>
+                    <p>
+                      {day.temp.min.toFixed(0)}° / {day.temp.max.toFixed(0)}°
+                    </p>
                   </div>
                 )
             )}
